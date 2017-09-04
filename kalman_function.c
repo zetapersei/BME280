5b67b58c8591b03acd6c94dbf4f63ba0b63afae3
Maurizio <zeta_persei@hotmail.com>
2017-09-04 10:54:19 +0200

    Create kalman_function.c
    A Kalman function prototipe.

---
+++ kalman_function.c
@@ -0,0 +1,18 @@
+float kalmanFilter(float inData)
+    {                        
+    static float prevData=0;
+    static float p=10, q=0.0001, r=0.05, kGain=0;
+ 
+    //Kalman filter function start*******************************
+    p = p+q;
+    kGain = p/(p+r);
+ 
+    inData = prevData+(kGain*(inData-prevData));
+ 
+    p = (1-kGain)*p;
+ 
+	prevData = inData;
+    //Kalman filter function stop********************************
+ 
+    return inData;
+    }
