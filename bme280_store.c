10eeb6069fbaa44af870a602a95431e3ce11af8b
Maurizio <zeta_persei@hotmail.com>
2017-09-04 11:13:06 +0200

    Update bme280_store.c

--- bme280_store.c
+++ bme280_store.c
@@ -29,6 +29,32 @@
 #include <fcntl.h>
 #include <mysql.h>
 
+//------------------------------------------------------------------
+//Kalman Filter
+//By Jed 2013/12/23
+//www.xuan.idv.tw
+//------------------------------------------------------------------
+
+float kalmanFilter(float inData)
+    {                        
+    static float prevData=0;
+    static float p=10, q=0.0001, r=0.05, kGain=0;
+ 
+    //Kalman filter function start*******************************
+    p = p+q;
+    kGain = p/(p+r);
+ 
+    inData = prevData+(kGain*(inData-prevData));
+ 
+    p = (1-kGain)*p;
+ 
+	prevData = inData;
+    //Kalman filter function stop********************************
+ 
+    return inData;
+    }
+// ------------------------------------------------------------------------------
+
 void main()
 {
 	// Mysql variables
